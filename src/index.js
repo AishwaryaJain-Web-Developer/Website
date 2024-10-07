c22036fd6d1e19150fe331ff54036b3fb1fe4dd8
Aishwarya Jain <aishwarya_jain@Aishwaryas-MacBook-Air.local>
2024-10-07 12:38:50 +0530

    deploye

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root'); // Get the root element
+const root = createRoot(container); // Create a root
+
 root.render(
   <React.StrictMode>
     <App />
